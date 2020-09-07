af9b26fdb32d820857c4d0c07cd2a98adaf88119
asianvader <phoebe.voong@gmail.com>
2020-09-07 11:02:15 +0100

    Set up Firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
-import IngredientList from './IngredientList';
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+import IngredientList from "./IngredientList";
 
 // function Ingredients() {
-const Ingredients = () => {  
-  const [ userIngredients, setUserIngredients] = useState([]);
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
 
-  const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ])
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-hooks-test-project-998b3.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((response) => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients((prevIngredients) => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient },
+      ]);
+    })
   };
 
-  const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients => prevIngredients.filter(item => item.id !== ingredientId))
-  }
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((item) => item.id !== ingredientId)
+    );
+  };
 
   return (
     <div className="App">
@@ -25,10 +38,13 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
